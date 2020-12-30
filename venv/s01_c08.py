fa5617c66827066d52342aec5d10206388f825ca
Logan Sullivan <logan.q.sullivan@gmail.com>
2020-12-29 18:25:45 -0600

    detect AES in ECB mode

---
+++ venv/s01_c08.py
@@ -0,0 +1,32 @@
+# detect AES in ECB mode
+# since 16-byte chunks always encode the same way, if we assume the true
+# plaintext has more repeated 2-hex blocks than the others, we can
+# just count those and find the one with the most
+
+def count_repetitions(input, n=2):
+    blocks = [input[i:i+n] for i in range(0, len(input), n)]
+
+    count = {}
+
+    # get counts
+    for b in blocks:
+        if b in count.keys():
+            count[b] += 1
+        else:
+            count[b] = 1
+
+    return count
+
+
+# read input
+if __name__ == '__main__':
+    ciphertexts = [text for text in open('s01_c08_input.txt').read().splitlines()]
+
+    total_reps = [None] * len(ciphertexts)
+
+
+    for i in range(len(ciphertexts)):
+        reps = count_repetitions(ciphertexts[i], 2)
+        total_reps[i] = sum(reps.values()) - len(reps)
+
+    [print(i, j) for i, j in enumerate(total_reps) if j == max(total_reps)]
