629d0b3244b831ad108340073a5ab5147ba5fd67
Logan Sullivan <logan.q.sullivan@gmail.com>
2020-12-31 22:19:41 -0600

    forgot to check the condition for PKCS#7 padding, it was just ripping things off every string

--- s02_c09.py
+++ s02_c09.py
@@ -14,10 +14,14 @@ def unpad(message, method = "PKCS#7"):
     if method == "PKCS#7":
         # pick value of last byte
         n_pad = int(message[len(message)-1].encode().hex(), 16)
-
+        # if message doesn't end in n_pad bytes with value n, it's not padded
+        # (coincidences possible but idk )
+        if message[(len(message)-n_pad):] != bytes([n_pad]*n_pad).decode('ascii'):
+            return message
     # remove that many bytes
     return message[:(len(message) - n_pad)]
 
 if __name__ == '__main__':
     assert pad("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
     assert unpad(pad("YELLOW SUBMARINE", 20)) == "YELLOW SUBMARINE"
+    assert unpad("YELLOW SUBMARINE\x03\x03") == "YELLOW SUBMARINE\x03\x03"
