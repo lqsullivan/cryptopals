05e46c1bc723d95e09a28e2c5f115e6912f93453
Logan Sullivan <logan.q.sullivan@gmail.com>
2020-12-31 23:43:37 -0600

    make pad and unpad handle strings and bytes types (might regret that later)

--- s02_c09.py
+++ s02_c09.py
@@ -1,23 +1,44 @@
 # PKCS#7 padding
 
 def pad(message, n, method = "PKCS#7"):
+    """
+    handles strings or bytes, maybe that's bad practice to do in one
+
+    :param message:
+    :param n:
+    :param method:
+    :return:
+    """
     if method == "PKCS#7":
         if n > 255:
             raise ValueError('For PKCS#7 padding, block size must be <256')
         # pad with number of bytes added
-        p = ((len(message) // n) + 1) * n
+        p = n - (len(message) % n)
 
-    return message.ljust(p, chr(p - len(message)))
+    if isinstance(message, str):
+        return message + ''.join([chr(p)] * p)
+    elif isinstance(message, bytes):
+        return message + bytes([p]) * p
+    else:
+        raise TypeError("pad doesn't know how to handle type " + type(message).__name__)
 
 
 def unpad(message, method = "PKCS#7"):
     if method == "PKCS#7":
-        # pick value of last byte
-        n_pad = int(message[len(message)-1].encode().hex(), 16)
-        # if message doesn't end in n_pad bytes with value n, it's not padded
-        # (coincidences possible but idk )
-        if message[(len(message)-n_pad):] != bytes([n_pad]*n_pad).decode('ascii'):
-            return message
+        if isinstance(message, str):
+            # pick value of last byte
+            n_pad = int(message[len(message) - 1].encode().hex(), 16)
+            # handle unpadded messages
+            if message[(len(message) - n_pad):] != bytes([n_pad] * n_pad).decode('ascii'):
+                return message
+        elif isinstance(message, bytes):
+            n_pad = message[len(message)-1]
+            # handle unpadded messages
+            if message[(len(message) - n_pad):] != bytes([n_pad] * n_pad):
+                return message
+        else:
+            raise TypeError("pad doesn't know how to handle type " + type(message).__name__)
+
     # remove that many bytes
     return message[:(len(message) - n_pad)]
 
@@ -25,3 +46,7 @@ if __name__ == '__main__':
     assert pad("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
     assert unpad(pad("YELLOW SUBMARINE", 20)) == "YELLOW SUBMARINE"
     assert unpad("YELLOW SUBMARINE\x03\x03") == "YELLOW SUBMARINE\x03\x03"
+
+    assert pad(b"YELLOW SUBMARINE", 20) == b"YELLOW SUBMARINE\x04\x04\x04\x04"
+    assert unpad(pad(b"YELLOW SUBMARINE", 20)) == b"YELLOW SUBMARINE"
+    assert unpad(b"YELLOW SUBMARINE\x03\x03") == b"YELLOW SUBMARINE\x03\x03"
