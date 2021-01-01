703d4919843a6b262e6f2235853512d6410c12ee
Logan Sullivan <logan.q.sullivan@gmail.com>
2020-12-31 22:12:13 -0600

    added unpad function

--- s02_c09.py
+++ s02_c09.py
@@ -1,22 +1,23 @@
 # PKCS#7 padding
 
-def pad(plaintext, n, method = "PKCS#7"):
-    """
-    pads to the nearest multiple of block size
-
-    :param plaintext:
-    :param n:
-    :param method:
-    :return:
-    """
-
+def pad(message, n, method = "PKCS#7"):
     if method == "PKCS#7":
         if n > 255:
             raise ValueError('For PKCS#7 padding, block size must be <256')
         # pad with number of bytes added
-        p = ((len(plaintext) // n) + 1) * n
+        p = ((len(message) // n) + 1) * n
 
-    return plaintext.ljust(p, chr(p - len(plaintext)))
+    return message.ljust(p, chr(p - len(message)))
+
+
+def unpad(message, method = "PKCS#7"):
+    if method == "PKCS#7":
+        # pick value of last byte
+        n_pad = int(message[len(message)-1].encode().hex(), 16)
+
+    # remove that many bytes
+    return message[:(len(message) - n_pad)]
 
 if __name__ == '__main__':
     assert pad("YELLOW SUBMARINE", 20) == "YELLOW SUBMARINE\x04\x04\x04\x04"
+    assert unpad(pad("YELLOW SUBMARINE", 20)) == "YELLOW SUBMARINE"
