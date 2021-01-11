284286f2f8b7e8bee7913ee934eb29391a40abe1
Logan Sullivan <logan.q.sullivan@gmail.com>
2021-01-10 22:32:51 -0600

    renamed strip_pad to unpad

--- s02_c15.py
+++ s02_c15.py
@@ -1,6 +1,6 @@
 # update PKCS#7 padding functions
 
-def strip_pad(message):
+def unpad(message):
     pad_char = message[len(message)-1]
     n = ord(pad_char)
 
@@ -10,6 +10,6 @@ def strip_pad(message):
         raise Exception("invalid message padding")
 
 if __name__ == '__main__':
-    assert strip_pad("ICE ICE BABY\x04\x04\x04\x04") == "ICE ICE BABY"
+    assert unpad("ICE ICE BABY\x04\x04\x04\x04") == "ICE ICE BABY"
     # should figure out how to unit test raised exceptions eventually
-    strip_pad("ICE ICE BABY\x01\x02\x03\x04")
+    unpad("ICE ICE BABY\x01\x02\x03\x04")
