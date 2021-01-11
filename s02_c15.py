adbcc3ef2fe92ebdc4158c27217d948341ebcdaa
Logan Sullivan <logan.q.sullivan@gmail.com>
2021-01-10 22:59:30 -0600

    now points to the OG unpad all in one place

--- s02_c15.py
+++ s02_c15.py
@@ -1,14 +1,4 @@
-# update PKCS#7 padding functions
-
-def unpad(message):
-    pad_char = message[len(message)-1]
-    n = ord(pad_char)
-
-    if message[len(message)-n:] == pad_char*n :
-        return message[:len(message)-n]
-    else:
-        raise Exception("invalid message padding")
-
+from s02_c09 import unpad
 
 if __name__ == '__main__':
     assert unpad("ICE ICE BABY\x04\x04\x04\x04") == "ICE ICE BABY"
