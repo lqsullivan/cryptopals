50963a4ae3db4cd0d5511bc5b53fbcb07c6d3d67
Logan Sullivan <logan.q.sullivan@gmail.com>
2021-01-11 23:10:48 -0600

    CBC oracle working, took some notes on what i think i should do next

--- s02_c16.py
+++ s02_c16.py
@@ -14,6 +14,32 @@ class CBCOracle(CBC):
         # overwrite the parent encrypt with a new one that prepares the string
         return super().encrypt(self.prep_string(plaintext))
 
+    def is_admin(self, ciphertext):
+        parsed = dict()
+        for key, value in [str.split('=') for str in self.decrypt(ciphertext).split(';')]:
+            parsed[key] = value
 
-cipher = CBCOracle(key='YELLOW SUBMARINE', iv='\x00'*16)
-cipher.encrypt('asdf')
+        if 'admin' in parsed.keys() and parsed['admin'] == 'true':
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    # test new class
+    cipher = CBCOracle(key='YELLOW SUBMARINE', iv='\x00'*16)
+    a = cipher.encrypt('asdf')
+    cipher.decrypt(a)
+    cipher.is_admin(a)
+
+    # modify the ciphertext to introduce an 'admin=true' block
+
+    # for CBC mode, 1-bit error in ciphertext block scrambles the block, 1 bit error in next block
+    # because the current block is block ciphered, 1 bit changes the whole thing (assuming good diffusion)
+    # the next block gets xor'd with the last ciphertext, so that 1 bit change makes the same 1-bit change
+
+    # so...i want to make 1-bit changes to ciphertext blocks
+    # then observe and lock the bits as the next block produces the message i want
+    # also have to find block boundaries first and adjust the input accordingly
+    # maybe i input user data as one block and try to get it to be '<16bitgibberish>AAAA;admin=true;'?
+
